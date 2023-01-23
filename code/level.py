6fb61410e28e19332ff52c15e297d3d05b16a218
mrmarlrico <mrmarlrico@gmail.com>
2023-01-23 12:55:50 -0800

    added floor tiles, objects and grass

--- code/level.py
+++ code/level.py
@@ -2,6 +2,9 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from support import import_csv_layout
+from support import import_folder
+from random import choice
 
 class Level:
     def __init__(self):
@@ -17,14 +20,34 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self._visible_sprites,self._obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y),[self._visible_sprites], self._obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout("../map/map_FloorBlocks.csv"),
+            'grass': import_csv_layout("../map/map_Grass.csv"),
+            'object': import_csv_layout("../map/map_Objects.csv")
+            
+        }
+        graphics = {
+            'grass': import_folder('../graphics/Grass'),
+            'objects': import_folder('../graphics/objects')
+        }
+        for style,layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        #  Create the boundary tile
+                        if style == 'boundary':
+                            Tile((x,y), [self._obstacle_sprites], 'invisible')
+                        # Create the grass tile
+                        if style == 'grass':
+                            random_grass_img = choice(graphics['grass'])
+                            Tile((x,y), [self._visible_sprites, self._obstacle_sprites], 'grass', random_grass_img)
+                        # Create the object tile
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y), [self._visible_sprites,self._obstacle_sprites],'object', surf)
+        self.player = Player((2000, 1400),[self._visible_sprites], self._obstacle_sprites)
 
 
     def run(self):
@@ -41,10 +64,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self._half_height = self._display_surface.get_size()[1] // 2
         self._offset = pygame.math.Vector2()
 
+        # Create the floor map
+        self._floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self._floor_rect = self._floor_surf.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
         # offset
         self._offset.x = player.rect.centerx - self._half_width
         self._offset.y = player.rect.centery - self._half_height
+
+        # Drawing the floor
+        floor_offset_pos = self._floor_rect.topleft - self._offset
+        self._display_surface.blit(self._floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self._offset
--- code/tile.py
+++ code/tile.py
@@ -2,8 +2,13 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, group):
+    def __init__(self, pos, group, sprite_type, surf = pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(group)
-        self.image = pygame.image.load("../graphics/test/rock.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self._sprite_type = sprite_type
+        self.image = surf
+        if sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft = (pos[0], pos[1]-TILESIZE))
+
+        else:
+            self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10)
